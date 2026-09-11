fd02d6d15368076eb3c5699a3e07b356ff8dd33e
Ninja <ninj@bytedojo>
2026-09-11 10:37:36 +0800

    refactor: name SOCKS5 protocol constants

--- pkg/instance/socks/client.go
+++ pkg/instance/socks/client.go
@@ -13,6 +13,23 @@ import (
 	"golang.org/x/crypto/ssh"
 )
 
+const (
+	socksVersion             = 0x05
+	socksMethodCount         = 0x01
+	socksNoAuthentication    = 0x00
+	socksCommandConnect      = 0x01
+	socksReserved            = 0x00
+	socksAddressTypeIPv4     = 0x01
+	socksAddressTypeDomain   = 0x03
+	socksAddressTypeIPv6     = 0x04
+	socksMaxDomainLength     = 255
+	socksMethodResponseBytes = 2
+	socksReplyHeaderBytes    = 4
+	socksIPv4AddressBytes    = 4
+	socksIPv6AddressBytes    = 16
+	socksPortBytes           = 2
+)
+
 // Client represents a SOCKS5 client that connects through an SSH channel
 type Client struct {
 	logger    *slog.Logger
@@ -93,30 +110,36 @@ func (c *Client) ConnectViaSocks(destination types.TcpIpChannelMsg, clientChanne
 // performHandshake performs a SOCKS5 handshake following RFC1928
 func (c *Client) performHandshake(socksChannel io.ReadWriter, destination types.TcpIpChannelMsg) error {
 	// Send SOCKS5 greeting: version 5, 1 method, no auth (0x00)
-	if _, err := socksChannel.Write([]byte{0x05, 0x01, 0x00}); err != nil {
+	if _, err := socksChannel.Write([]byte{socksVersion, socksMethodCount, socksNoAuthentication}); err != nil {
 		return fmt.Errorf("could not write socks5 greeting - %v", err)
 	}
 
 	// Read server response
-	respBuf := make([]byte, 2)
+	respBuf := make([]byte, socksMethodResponseBytes)
 	_, err := io.ReadFull(socksChannel, respBuf)
 	if err != nil {
 		return fmt.Errorf("could not read socks5 handshake response - %v", err)
 	}
 
 	// Verify response: version 5, method accepted (0x00)
-	if respBuf[0] != 0x05 || respBuf[1] != 0x00 {
+	if respBuf[0] != socksVersion || respBuf[1] != socksNoAuthentication {
 		return fmt.Errorf("invalid socks5 handshake response - %v", respBuf)
 	}
 
 	// Build connect request: version, command (CONNECT=1), reserved, address type (domain=3), host, port
-	if len(destination.DstHost) > 255 {
+	if len(destination.DstHost) > socksMaxDomainLength {
 		return fmt.Errorf("destination host exceeds SOCKS5 domain length")
 	}
-	req := []byte{0x05, 0x01, 0x00, 0x03, byte(len(destination.DstHost))}
+	req := []byte{
+		socksVersion,
+		socksCommandConnect,
+		socksReserved,
+		socksAddressTypeDomain,
+		byte(len(destination.DstHost)),
+	}
 	req = append(req, []byte(destination.DstHost)...)
 
-	portBytes := make([]byte, 2)
+	portBytes := make([]byte, socksPortBytes)
 	binary.BigEndian.PutUint16(portBytes, uint16(destination.DstPort))
 	req = append(req, portBytes...)
 
@@ -126,14 +149,14 @@ func (c *Client) performHandshake(socksChannel io.ReadWriter, destination types.
 	}
 
 	// Read connect response header
-	respHdr := make([]byte, 4)
+	respHdr := make([]byte, socksReplyHeaderBytes)
 	_, err = io.ReadFull(socksChannel, respHdr)
 	if err != nil {
 		return fmt.Errorf("could not read socks5 handshake connect response - %v", err)
 	}
 
 	// Verify connection success: version 5, status success (0x00)
-	if respHdr[0] != 0x05 || respHdr[1] != 0x00 {
+	if respHdr[0] != socksVersion || respHdr[1] != socksNoAuthentication {
 		return fmt.Errorf("unsuccessful socks5 handshake response - %v", respHdr)
 	}
 
@@ -141,21 +164,21 @@ func (c *Client) performHandshake(socksChannel io.ReadWriter, destination types.
 	var respBodyLen int
 	switch respHdr[3] {
 	// IPv4 address
-	case 0x01:
+	case socksAddressTypeIPv4:
 		// 4 bytes IPv4 + 2 bytes port
-		respBodyLen = 4 + 2
+		respBodyLen = socksIPv4AddressBytes + socksPortBytes
 	// Domain name
-	case 0x03:
+	case socksAddressTypeDomain:
 		domainLen := make([]byte, 1)
 		if _, err = io.ReadFull(socksChannel, domainLen); err != nil {
 			return fmt.Errorf("could not read socks5 domain length - %v", err)
 		}
 		// domain length + 2 bytes port
-		respBodyLen = int(domainLen[0]) + 2
+		respBodyLen = int(domainLen[0]) + socksPortBytes
 	// IPv6 address
-	case 0x04:
+	case socksAddressTypeIPv6:
 		// 16 bytes IPv6 + 2 bytes port
-		respBodyLen = 16 + 2
+		respBodyLen = socksIPv6AddressBytes + socksPortBytes
 	default:
 		return fmt.Errorf("unknown address type \"%v\" socks5 handshake response", respHdr[3])
 	}
